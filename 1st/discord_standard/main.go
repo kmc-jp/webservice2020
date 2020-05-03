c6b8d69ec23fae089a927d8b935f9d4686e0e827
TKMAX777 <mr.ultratkmax007@icloud.com>
2020-05-04 04:08:20 +0900

    discord_standard

---
+++ 1st/discord_standard/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+//Setting put setting data
+type Setting struct {
+	//DiscordToken put discord token
+	DiscordToken string `json:"discord"`
+}
+
+//Settings pit setting data
+var Settings Setting
+
+func init() {
+	b, e := ioutil.ReadFile(filepath.Join("credit.json"))
+	if e != nil {
+		panic(e.Error())
+	}
+	e = json.Unmarshal(b, &Settings)
+	if e != nil {
+		panic(e.Error())
+	}
+}
+func main() {
+	if Settings.DiscordToken == "" {
+		fmt.Println("No token provided. Please run: airhorn -t <bot token>")
+		return
+	}
+
+	// Create a new Discord session using the provided bot token.
+	dg, err := discordgo.New(Settings.DiscordToken)
+	if err != nil {
+		fmt.Println("Error creating Discord session: ", err)
+		return
+	}
+
+	dg.AddHandler(watch)
+
+	// Open the websocket and begin listening.
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("Error opening Discord session: ", err)
+	}
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Airhorn is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	dg.Close()
+}
+
+func watch(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	if m.Content != "いつやるの？" {
+		return
+	}
+
+	_, err := s.ChannelMessageSend(m.ChannelID, "今でしょ。")
+	if err != nil {
+		fmt.Printf("%s\n", err.Error())
+	}
+	return
+}
