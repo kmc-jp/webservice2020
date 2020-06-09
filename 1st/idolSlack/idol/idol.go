6ad6f3e5681ba93241b170fbf4f0546c4914c6e0
TKMacs <mr.ultratkmax007@icloud.com>
2020-06-10 02:25:02 +0900

    アイマスボット

---
+++ 1st/idolSlack/idol/idol.go
@@ -0,0 +1,148 @@
+package idol
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"path/filepath"
+	"strings"
+)
+
+//Idol put Idol info
+type Idol struct {
+	Type   string `json:"type"`
+	Name   string `json:"name"`
+	URL    string `json:"url"`
+	Age    string `json:"age"`
+	Birth  string `json:"birth"`
+	Star   string `json:"star"`
+	Blood  string `json:"blood"`
+	Height string `json:"height"`
+	Weight string `json:"weight"`
+	B      string `json:"b"`
+	W      string `json:"w"`
+	H      string `json:"j"`
+	Hand   string `json:"hand"`
+	Locate string `json:"locate"`
+	Hobby  string `json:"hobby"`
+	CV     string `json:"cv"`
+	Deploy string `json:"deploy"`
+}
+
+//Get gets wikipedia page text and title at random
+func Get() []Idol {
+	resp, err := http.Get("https://imascg-slstage-wiki.gamerch.com/%E3%82%A2%E3%82%A4%E3%83%89%E3%83%AB%E4%B8%80%E8%A6%A7")
+	if err != nil {
+		return []Idol{}
+	}
+	defer resp.Body.Close()
+
+	var buf *bytes.Buffer = new(bytes.Buffer)
+	io.Copy(buf, resp.Body)
+	var ret []byte = buf.Bytes()
+
+	var top []byte = bytes.Split(ret, []byte("id=\"content_1_2\""))[1]
+	var content string = string(bytes.Split(bytes.Split(bytes.Split(
+		top,
+		[]byte("id=\"content_1_2\""))[0],
+		[]byte("t-line-img word-keep-all"))[1],
+		[]byte("<tbody>"))[1],
+	)
+
+	var table []Idol
+
+	for i, x := range strings.Split(content, "<tr>") {
+		if i == 0 {
+			continue
+		}
+
+		var idol Idol
+
+		for _, y := range strings.Split(x, "<td") {
+			switch {
+			case strings.Contains(y, "data-col=\"1\""):
+				if len(strings.Split(y, ">")) < 2 || len(strings.Split(strings.Split(strings.Split(y, ">")[1], "</td")[0], "a href=\"")) < 2 {
+					continue
+				}
+				idol.URL = strings.Split(strings.Split(strings.Split(strings.Split(y, ">")[1], "</td")[0], "a href=\"")[1], "\"")[0]
+				idol.Name = strings.Split(strings.Split(y, "title=\"")[1], "\"")[0]
+			case strings.Contains(y, "data-col=\"2\""):
+				idol.Age = strings.Split(strings.Split(y, "data-col=\"2\">")[1], "</")[0]
+			case strings.Contains(y, "data-col=\"3\""):
+				idol.Birth = strings.Split(strings.Split(y, "data-col=\"3\">")[1], "</")[0]
+			case strings.Contains(y, "data-col=\"4\""):
+				idol.Star = strings.Split(strings.Split(y, "data-col=\"4\">")[1], "</")[0]
+			case strings.Contains(y, "data-col=\"5\""):
+				idol.Blood = strings.Split(strings.Split(y, "data-col=\"5\">")[1], "</")[0]
+			case strings.Contains(y, "data-col=\"6\""):
+				idol.Height = strings.Split(strings.Split(y, "data-col=\"6\">")[1], "</")[0]
+			case strings.Contains(y, "data-col=\"7\""):
+				if strings.Contains(y, "span") {
+					idol.Weight = strings.Split(strings.Split(y, "size8\">")[1], "</")[0]
+					continue
+				}
+				idol.Weight = strings.Split(strings.Split(y, "data-col=\"7\">")[1], "</")[0]
+
+			case strings.Contains(y, "data-col=\"8\""):
+				if len(strings.Split(y, "</span>")) < 2 {
+					continue
+				}
+				idol.B = strings.Split(strings.Split(y, "</span>")[1], "</")[0]
+			case strings.Contains(y, "data-col=\"9\""):
+				idol.W = strings.Split(strings.Split(y, "data-col=\"9\">")[1], "</")[0]
+			case strings.Contains(y, "data-col=\"10\""):
+				idol.H = strings.Split(strings.Split(y, "data-col=\"10\">")[1], "</")[0]
+			case strings.Contains(y, "data-col=\"11\""):
+				idol.Hand = strings.Split(strings.Split(y, "data-col=\"11\">")[1], "</")[0]
+			case strings.Contains(y, "data-col=\"12\""):
+				idol.Locate = strings.Split(strings.Split(y, "data-col=\"12\">")[1], "</")[0]
+			case strings.Contains(y, "data-col=\"13\""):
+				idol.Hobby = strings.Split(strings.Split(y, "data-col=\"13\">")[1], "</")[0]
+			case strings.Contains(y, "data-col=\"14\""):
+				idol.CV = strings.Split(strings.Split(y, "data-col=\"14\">")[1], "</")[0]
+			case strings.Contains(y, "data-col=\"15\""):
+				idol.Deploy = strings.Split(strings.Split(y, "data-col=\"15\">")[1], "</")[0]
+			}
+
+		}
+		table = append(table, idol)
+
+	}
+	fmt.Printf("%#v\n", table)
+	return table
+}
+
+//GetImgURL Get image url from URL page
+func GetImgURL(URL string) string {
+	resp, err := http.Get(URL)
+	if err != nil {
+		return ""
+	}
+	defer resp.Body.Close()
+
+	var buf *bytes.Buffer = new(bytes.Buffer)
+	io.Copy(buf, resp.Body)
+	var ret []byte = buf.Bytes()
+
+	var top []byte = bytes.Split(ret, []byte("id=\"content_1_1\""))[1]
+	var url []byte = bytes.Split(bytes.Split(top, []byte("a href=\""))[1], []byte("\""))[0]
+
+	return string(url)
+}
+
+//Read read idol data
+func Read() []Idol {
+	b, err := ioutil.ReadFile(filepath.Join("..", "data.json"))
+	if err != nil {
+		fmt.Printf("Error: Cannot Read file\n")
+		return []Idol{}
+	}
+
+	var info []Idol
+
+	json.Unmarshal(b, &info)
+	return info
+}
---
+++ 1st/idolSlack/main/main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"../idol"
+	"github.com/nlopes/slack"
+)
+
+//EV put new slack events
+var EV *slack.MessageEvent
+
+//RTM use for sending events to slack
+var RTM *slack.RTM
+
+//BotToken Put your slackbot token here
+const BotToken string = "YOUR_BOT_TOKEN"
+
+func init() {
+	var x []idol.Idol = idol.Get()
+	b, err := json.MarshalIndent(x, "", "    ")
+	if err != nil {
+		panic("Marshal Error!")
+	}
+	err = ioutil.WriteFile(filepath.Join("..", "data.json"), b, os.ModePerm)
+	if err != nil {
+		panic("Write Error!")
+	}
+}
+
+func main() {
+	// fmt.Printf("%#v\n", idol.Read())
+
+	var api *slack.Client = slack.New(BotToken)
+
+	RTM = api.NewRTM()
+
+	go RTM.ManageConnection()
+
+	for msg := range RTM.IncomingEvents {
+		switch ev := msg.Data.(type) {
+		case *slack.ConnectedEvent:
+			fmt.Printf("Start connection with Slack\n")
+		case *slack.MessageEvent:
+			EV = ev
+			ListenTo()
+		}
+	}
+
+}
+
+func ListenTo() {
+	switch {
+	case strings.Contains(EV.Text, "ちゃん"):
+		NameCheck()
+	case strings.Contains(EV.Text, "アイマスランダム"):
+		Random()
+	}
+}
+
+func Random() {
+	var Info []idol.Idol = idol.Read()
+	rand.Seed(time.Now().UnixNano())
+
+	var FoundNum int = int(rand.Float64() * float64(len(Info)))
+	var SendText string
+
+	SendText = "名前: " + Info[FoundNum].Name + "\n"
+	SendText += "年齢: " + Info[FoundNum].Age + "\n"
+	SendText += "身長: " + Info[FoundNum].Height + "\n"
+	SendText += "3Size: " + Info[FoundNum].B + "/" + Info[FoundNum].W + "/" + Info[FoundNum].H + "\n"
+	SendText += idol.GetImgURL(Info[FoundNum].URL) + "\n"
+	RTM.SendMessage(RTM.NewOutgoingMessage(SendText, EV.Channel))
+}
+
+func NameCheck() {
+	var Info []idol.Idol = idol.Read()
+	var NAME string = strings.Split(EV.Text, "ちゃん")[0]
+	var FoundNUM int
+
+	for i, info := range Info {
+		if info.Name == NAME {
+			FoundNUM = i
+			goto Found
+		}
+	}
+	return
+Found:
+	var SendText string
+	SendText = "名前: " + Info[FoundNUM].Name + "\n"
+	SendText += "年齢: " + Info[FoundNUM].Age + "\n"
+	SendText += "身長: " + Info[FoundNUM].Height + "\n"
+	SendText += "3Size: " + Info[FoundNUM].B + "/" + Info[FoundNUM].W + "/" + Info[FoundNUM].H + "\n"
+	SendText += "身長: " + idol.GetImgURL(Info[FoundNUM].URL) + "\n"
+	RTM.SendMessage(RTM.NewOutgoingMessage(SendText, EV.Channel))
+}
